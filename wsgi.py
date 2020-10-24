f200ea4bba00bb19284f8632395eb3760af2ef1b
Cesar de Barros <cesar.de.b.sorrilha@accenture.com>
2020-10-24 15:51:02 -0300

    fix: import error

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 """App entry point."""
 from application import create_app
+from os import environ
 
 app = create_app()
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
